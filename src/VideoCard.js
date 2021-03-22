d65d53643dbb59f0fa3ec5b84763bde8a99f9d73
tabufx50 <44902903+tabufx50@users.noreply.github.com>
2021-03-22 13:40:17 -0500

    added videocard and animation

---
+++ src/VideoCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './VideoCard.css';
+
+function VideoCard({movie}) {
+    return (
+        <div className='videoCard'>
+            <img src="" alt="" />
+            <p>This is a fil about coding..</p>
+            <h2>Movie Title</h2>
+            <p>Number of likes...</p>
+        </div>
+    )
+}
+
+export default VideoCard
